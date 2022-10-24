b0841702a47e2c1a15ee418894e2214265a290f5
Fan Yikun <LuMingJun71543@outlook.com>
2022-10-24 12:05:49 +0100

    Bug Fix - make action icons on movie card configurable.

--- movies/src/components/movieCard/index.js
+++ movies/src/components/movieCard/index.js
@@ -1,4 +1,4 @@
-import React, { useContext  } from "react";
+import React, { useContext } from "react";
 import { MoviesContext } from "../../contexts/moviesContext";
 import Avatar from '@mui/material/Avatar';
 import Card from "@mui/material/Card";
@@ -17,7 +17,7 @@ import img from '../../images/film-poster-placeholder.png'
 import { Link } from "react-router-dom";
 
 
-export default function MovieCard(movie) {
+export default function MovieCard({ movie, action }) {
   const { favorites, addToFavorites } = useContext(MoviesContext);
 
   if (favorites.find((id) => id === movie.id)) {
@@ -31,56 +31,54 @@ export default function MovieCard(movie) {
     addToFavorites(movie);
   };
 
-    return (
-      <Card sx={{ maxWidth: 345 }}>
-         <CardHeader
-          avatar={
-            movie.favorite ? (
-              <Avatar sx={{ backgroundColor: 'red' }}>
-                <FavoriteIcon />
-              </Avatar>
-            ) : null
-          }
-          title={
-            <Typography variant="h5" component="p">
-              {movie.title}{" "}
-            </Typography>
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          movie.favorite ? (
+            <Avatar sx={{ backgroundColor: 'red' }}>
+              <FavoriteIcon />
+            </Avatar>
+          ) : null
+        }
+        title={
+          <Typography variant="h5" component="p">
+            {movie.title}{" "}
+          </Typography>
         }
       />
-        <CardMedia
-          sx={{ height: 500 }}
-          image={
-            movie.poster_path
-              ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-              : img
-          }
-        />
-        <CardContent>
-          <Grid container>
-            <Grid item xs={6}>
-              <Typography variant="h6" component="p">
-                <CalendarIcon fontSize="small" />
-                {movie.release_date}
-              </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography variant="h6" component="p">
-                <StarRateIcon fontSize="small" />
-                {"  "} {movie.vote_average}{" "}
-              </Typography>
-            </Grid>
+      <CardMedia
+        sx={{ height: 500 }}
+        image={
+          movie.poster_path
+            ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+            : img
+        }
+      />
+      <CardContent>
+        <Grid container>
+          <Grid item xs={6}>
+            <Typography variant="h6" component="p">
+              <CalendarIcon fontSize="small" />
+              {movie.release_date}
+            </Typography>
           </Grid>
-        </CardContent>
-        <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites" onClick={handleAddToFavorite}>
-          <FavoriteIcon color="primary" fontSize="large" />
-        </IconButton>
-          <Link to={`/movies/${movie.id}`}>
-            <Button variant="outlined" size="medium" color="primary">
-              More Info ...
-            </Button>
-          </Link>
-        </CardActions>
+          <Grid item xs={6}>
+            <Typography variant="h6" component="p">
+              <StarRateIcon fontSize="small" />
+              {"  "} {movie.vote_average}{" "}
+            </Typography>
+          </Grid>
+        </Grid>
+      </CardContent>
+      <CardActions disableSpacing>
+        {action(movie)}
+        <Link to={`/movies/${movie.id}`}>
+          <Button variant="outlined" size="medium" color="primary">
+            More Info ...
+          </Button>
+        </Link>
+      </CardActions>
     </Card>
   );
 }
